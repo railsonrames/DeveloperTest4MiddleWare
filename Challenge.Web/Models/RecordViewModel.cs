4669ce3a7c5375c8c2e3e37fa131a62d245c7351
Railson Rames <rames.cloud@outlook.com.br>
2019-08-06 15:02:27 -0300

    Final ajusts to views.

---
+++ Challenge.Web/Models/RecordViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Challenge.Web.Models
+{
+    public class RecordViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool Status { get; set; }
+    }
+}
---
+++ Challenge.Web/wwwroot/js/Home/records.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function () {
+    $('#getDataButton').on('click', function () {
+        $.ajax({
+            type: "GET",
+            url: "http://localhost:9001/api/records/list/",
+            success: function (data) {
+                $('#insertPoint').html(setDataTable(data));
+            }
+        });
+    });
+});
+
+function setDataTable(dataReceived) {
+    var html = '<table class="table text-left mt-5"><thead><tr><th>Id</th><th>Name</th><th>Status</th></tr></thead><tbody>';
+    $.each(dataReceived, function (innerCounter, dataItem) {
+        html += `<tr><td>${dataItem.id}</td><td>${dataItem.name}</td><td>${dataItem.status}</td></tr>`;
+    });
+    html += '</tbody></table>';
+    return html;
+};
