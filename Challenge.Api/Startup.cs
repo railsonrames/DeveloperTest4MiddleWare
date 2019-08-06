850e8a25e47eefd4d3c9401ec67764a150d0c311
Railson Rames <rames.cloud@outlook.com.br>
2019-08-06 14:32:54 -0300

    Api done.

--- Challenge.Api/Startup.cs
+++ Challenge.Api/Startup.cs
@@ -1,40 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace Challenge.Api
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+        public Startup(IConfiguration configuration) => Configuration = configuration;
 
         public IConfiguration Configuration { get; }
+        readonly string AllowedSpecificOrigins = "_AllowedSpecificOrigins";
+        readonly string[] SpecifcUrlOriginsList = { "http://localhost:9000" };
+        readonly string[] SpecificHtmlVerbs = { "GET" };
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options => {
+                options.AddPolicy(AllowedSpecificOrigins, builder =>
+                    builder.WithOrigins(SpecifcUrlOriginsList)
+                           .WithMethods(SpecificHtmlVerbs));
+            });
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
-            {
                 app.UseDeveloperExceptionPage();
-            }
 
+            app.UseCors(AllowedSpecificOrigins);
             app.UseMvc();
         }
     }
